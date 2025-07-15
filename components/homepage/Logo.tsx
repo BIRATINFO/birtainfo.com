a1d1afffd734a4c0cd18a666faf03a75df94e113
joceesagar <joshisagarm2@gmail.com>
2025-07-15 17:29:36 +0545

    changed logo and favicon icon

--- components/homepage/Logo.tsx
+++ components/homepage/Logo.tsx
@@ -8,7 +8,7 @@ export default function Logo() {
             <div className='relative h-full flex-[1]'>
                 <Image
                     src='/logo.svg'
-                    alt='DIgitalKoshi Logo'
+                    alt='BirtaInfo Logo'
                     fill
                     className='object-contain'
                     priority
@@ -16,8 +16,8 @@ export default function Logo() {
             </div>
             <div className='relative h-full' style={{ width: '70%' }}>
                 <Image
-                    src='/DIGITALKOSHI.svg'
-                    alt='DigitalKoshi Text Logo'
+                    src='/BIRTAINFO.svg'
+                    alt='BirtaInfo Text Logo'
                     fill
                     className='object-contain'
                     priority
--- components/homepage/MobileHeader.tsx
+++ components/homepage/MobileHeader.tsx
@@ -42,7 +42,7 @@ export default function MobileHeader({
                     <div className='relative h-10 w-[40px]'>
                         <Image
                             src='/logo.svg'
-                            alt='DigitalKoshi Logo'
+                            alt='BirtaInfo Logo'
                             fill
                             className='object-contain'
                             priority
@@ -50,8 +50,8 @@ export default function MobileHeader({
                     </div>
                     <div className='relative h-10 w-[120px]'>
                         <Image
-                            src='/DIGITALKOSHI.svg'
-                            alt='DigitalKoshi Text Logo'
+                            src='/BIRTAINFO.svg'
+                            alt='BirtaInfo Text Logo'
                             fill
                             className='object-contain'
                             priority
